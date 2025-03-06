8ee35a667c4ca6c14da31cd3334999f87912ddc3
Mattias <Freezard@users.noreply.github.com>
2025-03-06 18:36:37 +0100

    2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,32 +1,4 @@
-const Header = ({ course }) => <h3>{course}</h3>
-
-const Content = ({ parts }) => (
-  <div>
-    {parts.map(part => <Part key={part.id} part={part} />)}
-  </div>
-)
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-)
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((acc, cur) => acc + cur.exercises, 0)
-
-  return <p><b>total of {total} exercises</b></p>
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,31 @@
+const Header = ({ course }) => <h3>{course}</h3>
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => <Part key={part.id} part={part} />)}
+  </div>
+)
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((acc, cur) => acc + cur.exercises, 0)
+
+  return <p><b>total of {total} exercises</b></p>
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
