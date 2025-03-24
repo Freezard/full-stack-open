4fc5bbba5975283cb8a53a935ad82250d6213d4e
Mattias <Freezard@users.noreply.github.com>
2025-03-24 18:16:53 +0100

    3.11

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -6,6 +6,7 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 
 const app = express()
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
