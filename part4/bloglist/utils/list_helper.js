0d1723e170400e0abdd7f020c549aa3cf6b6901d
Mattias <Freezard@users.noreply.github.com>
2025-04-01 16:56:57 +0200

    4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const sumLikes = (acc, cur) => {
+    return acc + cur.likes
+  }
+
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce(sumLikes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
