06e1ae122d012cfed14f842f939249f6c6b65db0
Mattias <Freezard@users.noreply.github.com>
2025-04-08 12:23:43 +0200

    4.15

--- part4/bloglist/tests/test_helper.js
+++ part4/bloglist/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const listWithOneBlog = [
     {
@@ -62,9 +63,14 @@ const listWithOneBlog = [
   }  
 ]
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(user => user.toJSON())
+}
+
 const blogsInDb = async () => {
   const blogs = await Blog.find({})
   return blogs.map(blog => blog.toJSON())
 }
 
-module.exports = { listWithOneBlog, listWithSeveralBlogs, blogsInDb }
+module.exports = { listWithOneBlog, listWithSeveralBlogs, blogsInDb, usersInDb }
