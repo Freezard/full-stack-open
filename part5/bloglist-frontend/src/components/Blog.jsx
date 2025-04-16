d974937b0279578c0a03b90e412a92c8b94b3266
Mattias <Freezard@users.noreply.github.com>
2025-04-16 16:00:46 +0200

    5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,31 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showInfoVisible, setShowInfoVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const hideWhenVisible = { display: showInfoVisible ? 'none' : '' }
+  const showWhenVisible = { display: showInfoVisible ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        {blog.title} {blog.author} <button onClick={() => setShowInfoVisible(true)}>view</button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.title} {blog.author} <button onClick={() => setShowInfoVisible(false)}>hide</button><br />
+        {blog.url}<br />
+        likes {blog.likes} <button>like</button><br />
+        {blog.user.name}
+      </div>
+  </div>
+)}
 
 export default Blog
