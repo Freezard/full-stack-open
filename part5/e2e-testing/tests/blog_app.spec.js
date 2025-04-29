90b8b45f6dfd93b08a08342825419334abfe0b82
Mattias <Freezard@users.noreply.github.com>
2025-04-29 16:46:45 +0200

    5.19

--- part5/e2e-testing/tests/blog_app.spec.js
+++ part5/e2e-testing/tests/blog_app.spec.js
@@ -33,11 +33,29 @@ describe('Blog app', () => {
       await page.getByTestId('password').fill('wrong')
       await page.getByRole('button', { name: 'login' }).click()
 
-      const notificationDiv = await page.locator('.notification')
+      const notificationDiv = page.locator('.notification')
       await expect(notificationDiv).toContainText('invalid username or password')
       await expect(notificationDiv).toHaveCSS('border-style', 'solid')
       await expect(notificationDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
       await expect(page.getByText('Matti Luukkainen logged in')).not.toBeVisible()
+    })
+  })
+
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      await page.getByTestId('username').fill('mluukkai')
+      await page.getByTestId('password').fill('salainen')
+      await page.getByRole('button', { name: 'login' }).click()
+    })
+
+    test('a new blog can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'new blog' }).click()
+      await page.getByPlaceholder('Title').fill('My first blog')
+      await page.getByPlaceholder('Author').fill('Dan Roswell')
+      await page.getByPlaceholder('URL').fill('urlthis')
+      await page.getByRole('button', { name: 'create' }).click()
+      await expect(page.locator('.notification'))
+        .toContainText('New blog added: My first blog by Dan Roswell')
     })
   })
 })
